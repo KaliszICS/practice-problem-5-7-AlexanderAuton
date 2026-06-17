8e5155894c8c831310801982b84097c6a6ba0143
AlexanderAuton <350319867@tdsb.ca>
2026-06-17 00:40:59 +0000

    My Semifinal (I dont have time to do 5.9 I calculated)

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,44 @@
+/**
+	* File: Lesson 5.7 - arrayLists
+	* Author: Auton
+	* Date Created: May 06, 2026
+	* Date Last Modified: Jun 16, 2026
+*/	
+import java.util.ArrayList;
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static void main(String args[]) {
 
-	}
+    }
 
+    public static void swap(ArrayList<String> arr, int index1, int index2) {
+        String temp = arr.get(index1);
+        arr.set(index1, arr.get(index2));
+        arr.set(index2, temp);
+    }
+
+    public static ArrayList<Double> createArrayList(double[] arr) {
+        ArrayList<Double> list = new ArrayList<Double>();
+
+        for (int i = 0; i < arr.length; i++) {
+            list.add(arr[i]);
+        }
+
+        return list;
+    }
+
+    public static ArrayList<Integer> mergeLists(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        ArrayList<Integer> newList = new ArrayList<Integer>();
+
+        for (int i = 0; i < list1.size(); i++) {
+            newList.add(list1.get(i));
+        }
+
+        for (int i = 0; i < list2.size(); i++) {
+            newList.add(list2.get(i));
+        }
+
+        return newList;
+    }
 }
